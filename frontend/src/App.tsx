042c45b8dca816e4f9f56b1ac3213f79b574dce9
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-29 10:44:54 -0300

    Axios toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
 import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  
+
 
   return (
     <>
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
 
-    <Header />
-    <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-         
-        </div>
-
-      </section>
-    </main>
-  </>
+        </section>
+      </main>
+    </>
   )
 }
 
