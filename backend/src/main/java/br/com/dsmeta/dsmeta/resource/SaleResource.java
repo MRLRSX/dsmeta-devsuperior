844b462686620e9d15f53bffe97043ee88096611
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-29 05:21:39 -0300

    ENDPOINT ENVIO SMS

--- backend/src/main/java/br/com/dsmeta/dsmeta/resource/SaleResource.java
+++ backend/src/main/java/br/com/dsmeta/dsmeta/resource/SaleResource.java
@@ -2,15 +2,13 @@ package br.com.dsmeta.dsmeta.resource;
 
 import br.com.dsmeta.dsmeta.entities.Sale;
 import br.com.dsmeta.dsmeta.service.SaleService;
+import br.com.dsmeta.dsmeta.service.SmsServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sales")
@@ -18,14 +16,22 @@ public class SaleResource {
 
     @Autowired
     private SaleService service;
+    @Autowired
+    private SmsServices smsService;
 
     @GetMapping
     public ResponseEntity<Page<Sale>> findSales(
-            @RequestParam(value="minDate", defaultValue = "") String minDate,
-            @RequestParam(value="maxDate", defaultValue = "") String maxDate,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             @PageableDefault(size = 12) Pageable pageable) {
 
         Page<Sale> lista = service.findSales(minDate, maxDate, pageable);
         return ResponseEntity.ok().body(lista);
+    }
+
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
