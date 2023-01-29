969709593336040673b3a27784af6c10cfd2dd12
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-29 09:51:39 -0300

    Project Frontend Dsmeta

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+
+function App() {
+  
+
+  return (
+    <>
+
+    <Header />
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+         
+        </div>
+
+      </section>
+    </main>
+  </>
+  )
+}
+
+export default App
